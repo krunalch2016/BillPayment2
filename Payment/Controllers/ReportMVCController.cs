1113cdbd73b1eaf99843eda2b6dbabfc2038aa85
changawalakrunal@gmail.com <changawalakrunal@gmail.com>
2019-11-20 01:29:40 +0530

    Add report MVC controller and few changes in layout controller

---
+++ Payment/Controllers/ReportMVCController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Payment.Controllers
+{
+    public class ReportMVCController : Controller
+    {
+        // GET: ReportMVC
+        public ActionResult ReportView()
+        {
+            return View();
+        }
+    }
+}
